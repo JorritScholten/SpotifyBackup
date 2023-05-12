77ed6d0a5c66a655a3dafc447970531ad4eb51a9
Jorrit Scholten <jorrit.scholten@hotmail.com>
2023-05-12 18:19:09 +0200

    progress on rewrite or parseArguments()
    TODO: rewrite parser() so that it uses an iterator instead of an index position

--- app/src/main/java/spotifybackup/cmd/CmdParser.java
+++ app/src/main/java/spotifybackup/cmd/CmdParser.java
@@ -2,7 +2,7 @@ package spotifybackup.cmd;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.function.Supplier;
+import java.util.regex.Pattern;
 
 public class CmdParser {
     private final List<Argument> arguments;
@@ -13,7 +13,7 @@ public class CmdParser {
         this.arguments.addAll(List.of(arguments));
     }
 
-    public void parseArguments(String[] args)
+    public void parseArguments(final String[] args)
             throws MissingArgumentException, ArgumentNotPresentException, MalformedInputException {
         if (!argumentsParsed) {
             Argument lookingForArgument = null;
@@ -70,5 +70,73 @@ public class CmdParser {
                     .orElseThrow(() -> new ArgumentNotPresentException("Argument " + name + " not supplied."))
                     .getValue();
         }
+    }
+
+    private Argument identifyArgumentByShortName(char c) {
+        return arguments.stream().filter(argument -> (argument.shortName == c))
+                .findFirst().orElse(null);
+    }
+
+    private void parser(final LexedArgs[] args) throws MalformedInputException {
+        List.of(args).stream().iterator()
+        for (int i = 0; i < args.length; i++) {
+            switch (args[i].type) {
+                case SHORT_ARGUMENT -> {
+                    char c = args[i].arg.charAt(1);
+                    var argument = identifyArgumentByShortName(c);
+                    if (argument == null) {
+                        throw new MalformedInputException("No argument defined by: " + c);
+                    } else if (!argument.isPresent) {
+                        argument.isPresent = true;
+                        if (argument.hasValue) {
+                            if (args[i + 1].type == ArgType.VALUE) {
+                                argument.setValue(args[i + 1].arg);
+                                i++; // skip next argument because it will be a VALUE type
+                            } else {
+                                throw new MalformedInputException("Argument " + argument.name + " supplied without value.");
+                            }
+                        }
+                    } else {
+                        throw new MalformedInputException("Argument " + argument.name + " repeated more than once in input.");
+                    }
+                }
+
+                case VALUE -> {
+                    throw new MalformedInputException("Value: " + args[i].arg + " supplied without identifying argument.");
+                }
+            }
+        }
+    }
+
+    private LexedArgs[] lexer(final String[] args) throws MalformedInputException {
+        var retval = new LexedArgs[args.length];
+        for (int i = 0; i < args.length; i++) {
+            for (ArgType a : ArgType.values()) {
+                if (a.regex.matcher(args[i]).find()) {
+                    retval[i] = new LexedArgs(args[i], a);
+                    break;
+                }
+            }
+            if (retval[i] == null) {
+                throw new MalformedInputException("Argument not identified as an ArgType.");
+            }
+        }
+        return retval;
+    }
+
+    private enum ArgType {
+        SHORT_ARGUMENT("^[-]{1}[\\w]{1}$"),
+        SHORT_ARGUMENTS("^[-]{1}[\\w]{2,}$"),
+        LONG_ARGUMENT("^[-]{2}[\\w]+$"),
+        VALUE("^[^-]{1,2}[\\w]+$");
+
+        final Pattern regex;
+
+        ArgType(String regex) {
+            this.regex = Pattern.compile(regex);
+        }
+    }
+
+    record LexedArgs(String arg, ArgType type) {
     }
 }
