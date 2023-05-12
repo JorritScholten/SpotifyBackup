f9eda65f0a17177a84b6b558982a9abfa8463e89
Jorrit Scholten <jorrit.scholten@hotmail.com>
2023-05-12 18:19:10 +0200

    wrote javadoc comments for MandatoryIntArgument.java

--- app/src/main/java/spotifybackup/cmd/MandatoryIntArgument.java
+++ app/src/main/java/spotifybackup/cmd/MandatoryIntArgument.java
@@ -3,11 +3,26 @@ package spotifybackup.cmd;
 public class MandatoryIntArgument extends Argument {
     private Integer value;
 
+    /**
+     * Mandatory integer argument, program won't execute if missing.
+     * @param name        Identifying name of argument, --{name} is used as identifier.
+     * @param description Description of argument printed in help.
+     * @param shortName   Identifying character of argument, -{Character} is used as identifier.
+     * @throws IllegalConstructorParameterException When trying assign name as null or assigning shortName a character
+     *                                              not in the alphabet.
+     */
     public MandatoryIntArgument(String name, String description, Character shortName)
             throws IllegalConstructorParameterException {
         super(name, description, shortName, true, true);
     }
 
+    /**
+     * Mandatory integer argument without identifying character, program won't execute if missing.
+     * @param name        Identifying name of argument, --{name} is used as identifier.
+     * @param description Description of argument printed in help.
+     * @throws IllegalConstructorParameterException When trying assign name as null or assigning shortName a character
+     *                                              not in the alphabet.
+     */
     public MandatoryIntArgument(String name, String description) {
         super(name, description, true, true);
     }
