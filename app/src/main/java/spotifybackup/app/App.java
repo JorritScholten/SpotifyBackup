a3426921ad32f50bc112e3fefb1629b9435e7984
Jorrit Scholten <jorrit.scholten@hotmail.com>
2023-05-27 14:44:20 +0200

    refactored App.java to use builder pattern

--- app/src/main/java/spotifybackup/app/App.java
+++ app/src/main/java/spotifybackup/app/App.java
@@ -1,37 +1,22 @@
 package spotifybackup.app;
 
-import spotifybackup.cmd.*;
-import spotifybackup.cmd.argument.file.MandatoryFilePathArgument;
-import spotifybackup.cmd.argument.integer.DefaultIntArgument;
+import spotifybackup.cmd.CmdParser;
 import spotifybackup.cmd.argument.integer.MandatoryIntArgument;
-import spotifybackup.cmd.argument.string.DefaultStringArgument;
-import spotifybackup.cmd.exception.MalformedInputException;
-import spotifybackup.cmd.exception.MissingArgumentException;
 
 public class App {
     public static void main(String[] args) {
         addition(args);
-//        System.out.println("Hello SpotifyBackup!");
-//        CmdParser argParser = new CmdParser(new Argument[]{
-//                new MandatoryIntArgument("int", "some integer"),
-//                new DefaultIntArgument("int2", "Lorem ipsum dolor sit amet, consectetur adipiscing " +
-//                        "elit, sed do eiusmod tempor incididunt ut", 'i', 23),
-//                new DefaultStringArgument("str", "some sort of string, dunno, not gonna use it.",
-//                        's', "string"),
-//                new MandatoryFilePathArgument("txt", "Lorem ipsum dolor sit amet, consectetur " +
-//                        "adipiscing elit, sed do eiusmod tempor incididunt ut", false)
-//        }, "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut",
-//                "testName.jar", "labore et dolore magna aliqua. Ut enim ad minim veniam, quis " +
-//                "nostrud exercitation");
-////        argParser.parseArguments(args);
-//        System.out.println(argParser.getHelp(60));
     }
 
     public static void addition(String[] args) {
-        var argParser = new CmdParser(new Argument[]{
-                new MandatoryIntArgument("value1", "First value.", 'a'),
-                new MandatoryIntArgument("value2", "Second value.", 'b')
-        }, "Program description", "Add.jar", "Help footer");
+        var argParser = new CmdParser.Builder()
+                .argument(new MandatoryIntArgument("value1", "First value.", 'a'))
+                .argument(new MandatoryIntArgument("value2", "Second value.", 'b'))
+                .description("Program description")
+                .programName("Add.jar")
+                .epilogue("Help footer")
+                .addHelp()
+                .build();
         try {
             argParser.parseArguments(args);
             if (argParser.isPresent("help")) {
