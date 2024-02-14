7a250b9debc3fc02559d41be2627374d69f641cf
Jorrit Scholten <jorrit.scholten@hotmail.com>
2024-02-14 14:54:22 +0100

    created test data and fixed test for SpotifySavedTracksRepositoryTest
    Creating a Paging<> object from .json file wasn't working and also unneeded so switching to a SavedTrack[] object was easiest.

--- app/src/test/java/spotifybackup/storage/SpotifySavedTracksRepositoryTest.java
+++ app/src/test/java/spotifybackup/storage/SpotifySavedTracksRepositoryTest.java
@@ -3,7 +3,6 @@ package spotifybackup.storage;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.condition.EnabledIfEnvironmentVariable;
-import se.michaelthelin.spotify.model_objects.specification.Paging;
 import se.michaelthelin.spotify.model_objects.specification.SavedTrack;
 import se.michaelthelin.spotify.model_objects.specification.User;
 
@@ -14,7 +13,7 @@ import java.nio.file.Path;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @EnabledIfEnvironmentVariable(named = "EnableStorageTests", matches = "true")
- class SpotifySavedTracksRepositoryTest {
+class SpotifySavedTracksRepositoryTest {
     static final String testDataDir = "src/test/java/spotifybackup/storage/spotify_api_get/";
     static final String userDir = testDataDir + "user/";
     static final String libraryDir = testDataDir + "library/";
@@ -32,15 +31,15 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
                 new String(Files.readAllBytes(Path.of(userDir + "user.json")))
         );
         final var user = spotifyObjectRepository.persist(apiUser);
-        final Paging<SavedTrack> apiSavedTracks = new Paging.JsonUtil<SavedTrack>().createModelObject(
-                new String(Files.readAllBytes(Path.of(libraryDir + "testaccount_saved_tracks.json")))
+        final SavedTrack[] apiSavedTracks = new SavedTrack.JsonUtil().createModelObjectArray(
+                new String(Files.readAllBytes(Path.of(libraryDir + "testaccount_saved_tracks.json"))), "items"
         );
-        final long savedTrackCountOld = spotifyObjectRepository.countSavedTracks(user);
+        assertEquals(0, spotifyObjectRepository.countSavedTracks(user));
 
         // Act
-        spotifyObjectRepository.persist(apiSavedTracks.getItems());
+        spotifyObjectRepository.persist(apiSavedTracks, user);
 
         // Assert
-        assertEquals(apiSavedTracks.getTotal() + savedTrackCountOld, spotifyObjectRepository.countSavedTracks(user));
+        assertEquals(apiSavedTracks.length, spotifyObjectRepository.countSavedTracks(user));
     }
 }
